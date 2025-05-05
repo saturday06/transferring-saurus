ea6f0074c2b3e5a080f2bcdfc63f3dbaa2148a7f
Isamu Mogi <doyoubi@pixiv.co.jp>
2025-05-05 09:39:41 +0900

    o

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "../../src/components/HomepageFeatures/index.tsx";
+import HomepageFeatures from "../components/HomepageFeatures/index.tsx";
 import Heading from "@theme/Heading";
 import React from "react";
 import styles from "./index.module.css";
